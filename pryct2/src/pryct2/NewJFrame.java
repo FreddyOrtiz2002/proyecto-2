7c6ecfdf639fb520d9c37e8ed9256a86ef05ae16
FreddyOrtiz2002 <60480312+FreddyOrtiz2002@users.noreply.github.com>
2020-03-10 20:22:23 -0500

    se desarrolla el programa del ahorcado

--- pryct2/src/pryct2/NewJFrame.java
+++ pryct2/src/pryct2/NewJFrame.java
@@ -1,11 +1,15 @@
 package pryct2;
 
+import java.awt.Panel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class NewJFrame extends javax.swing.JFrame {
     public String clave;
-    public short intentos=9;
+    public short intentos;
+    public short contador1=0;
     public NewJFrame() {
         setLocation(450, 200);
         initComponents();
@@ -22,6 +26,8 @@ public class NewJFrame extends javax.swing.JFrame {
         txtbuscar = new javax.swing.JTextField();
         botbusq = new javax.swing.JButton();
         vidas = new javax.swing.JLabel();
+        letras = new javax.swing.JLabel();
+        dibujo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,31 +57,35 @@ public class NewJFrame extends javax.swing.JFrame {
             }
         });
 
-        vidas.setText("vidas: ");
+        letras.setText("letras que no estan:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(80, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(palabraclave, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1))
-                        .addGap(26, 26, 26)
-                        .addComponent(bot1))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(50, 50, 50)
+                        .addComponent(letras, javax.swing.GroupLayout.PREFERRED_SIZE, 315, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(26, 26, 26)
-                        .addComponent(botbusq)))
-                .addGap(73, 73, 73))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(89, 89, 89)
-                .addComponent(vidas, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(30, 30, 30)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(palabraclave, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel1))
+                                .addGap(26, 26, 26)
+                                .addComponent(bot1))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 375, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(26, 26, 26)
+                                .addComponent(botbusq))
+                            .addComponent(vidas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 65, Short.MAX_VALUE)
+                .addComponent(dibujo, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(64, 64, 64))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -83,25 +93,35 @@ public class NewJFrame extends javax.swing.JFrame {
                 .addGap(26, 26, 26)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(palabraclave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(bot1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(botbusq))
-                .addGap(18, 18, 18)
-                .addComponent(vidas, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(palabraclave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(bot1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(botbusq))
+                        .addGap(18, 18, 18)
+                        .addComponent(vidas, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(letras, javax.swing.GroupLayout.DEFAULT_SIZE, 57, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(dibujo, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void bot1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bot1ActionPerformed
+        contador1=0;
         int tamañoP;
+        intentos=9;
+        vidas.setText("vidas: "+intentos);
         tamañoP=palabraclave.getText().length();//sacar el tamaño de la palabra
         int cols=tamañoP;//se saca las columnas
         int rows=1;//se saca las filas
@@ -112,40 +132,80 @@ public class NewJFrame extends javax.swing.JFrame {
         {
             tabla.setValueAt("letra"+(columna+1), 0, columna);
         }        
-        vidas.setText(vidas.getText()+Integer.toString(intentos));
+        for(int i=0;i<clave.length();i++)
+        {
+            if(clave.charAt(i)==' ')
+            {
+                tabla.setValueAt("_", 1, i);
+                contador1++;
+            }
+        }
     }//GEN-LAST:event_bot1ActionPerformed
-
     private void botbusqActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botbusqActionPerformed
             char caracteres[]=new char[clave.length()];
             for(int indice=0;indice<clave.length();indice++)
             {
                 caracteres[indice]=clave.charAt(indice);
             }
-            short fallo=0;
-            for(int j=0;j<clave.length();j++)
+            short fallo=0,contador=0;
+            if(txtbuscar.getText().length()==1)
             {
-                for(int indice=0;indice<txtbuscar.getText().length();indice++)
+                for(int j=0;j<clave.length();j++)
                 {
-                    if(txtbuscar.getText().charAt(indice)==caracteres[j])
+                        if(txtbuscar.getText().charAt(0)==caracteres[j])
+                        {
+                            tabla.setValueAt(txtbuscar.getText().charAt(0), 1, j);
+                            contador1++;
+                        }else
+                        {
+                            fallo++;
+                        }
+                }
+                if(fallo==clave.length())
+                {
+                    intentos--;
+                    vidas.setText("vidas: "+intentos);
+                    letras.setText(letras.getText()+" "+txtbuscar.getText().charAt(0));
+                }
+                txtbuscar.setText("");
+                if(contador1==clave.length())
+                {
+                    vidas.setText("GANASTE");
+                }  
+                if(intentos==0)
+                {
+                    vidas.setText("PERDISTE LA PALABRA ERA: "+clave);
+                }
+            }else
+            {
+                for(int j=0;j<clave.length();j++)
+                {
+                    for(int i=0;i<txtbuscar.getText().length();i++)
                     {
-                        tabla.setValueAt(txtbuscar.getText().charAt(indice), 1, j);
-                    }else
-                    {
-                        fallo++;
+                    if(txtbuscar.getText().charAt(i)==caracteres[j])
+                        {
+                            contador++;
+                        }else
+                        {
+                            fallo++;
+                        }
                     }
                 }
+                
+                if(contador==clave.length())
+                {
+                    for(int j=0;j<clave.length();j++)
+                    {
+                        tabla.setValueAt(clave.charAt(j), 1, j);
+                    }
+                    vidas.setText("GANASTE");
+                    txtbuscar.setText("");
+                }
+                if(intentos==0)
+                {
+                    vidas.setText("PERDISTE LA PALABRA ERA: "+clave);
+                }
             }
-            JOptionPane.showMessageDialog(null, fallo);
-            JOptionPane.showMessageDialog(null, txtbuscar.getText().length());
-            
-            
-            
-            
-        
-        
-        
-        
-        
     }//GEN-LAST:event_botbusqActionPerformed
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -157,14 +217,15 @@ public class NewJFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bot1;
     private javax.swing.JButton botbusq;
+    private javax.swing.JLabel dibujo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel letras;
     private javax.swing.JPasswordField palabraclave;
     private javax.swing.JTable tabla;
     private javax.swing.JTextField txtbuscar;
     private javax.swing.JLabel vidas;
     // End of variables declaration//GEN-END:variables
-
     private String String(short intentos) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
